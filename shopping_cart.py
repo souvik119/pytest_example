d9511554d32aafc818f7768b96c7c87ef4f8b2ed
“souvik119” <911.souvik@gmail.com>
2023-03-16 15:45:04 -0700

    basic class structure for shopping cart

---
+++ shopping_cart.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class ShoppingCart:
+
+	def __init__(self):
+		pass
+
+	def add(self, item):
+		pass
+
+	def size(self):
+		return 0
+
+	def get_items(self):
+		pass
+
+	def get_total_price(self, price_map):
+		pass
+		
